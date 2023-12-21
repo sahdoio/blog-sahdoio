352884a31b00c7e48941b8e87346409869c891b8
Lucas Sahdo <lucassahdo@gmail.com>
2023-12-21 15:53:22 -0300

    More update to coming soon page

--- src/resources/views/livewire/coming.blade.php
+++ src/resources/views/livewire/coming.blade.php
@@ -3,20 +3,20 @@
         <div class="logo-wrapper">
             <img src="{{ asset('images/logo.png') }}" alt="SahdoIO"/>
         </div>
-        <h1>coming soon<span class="dot">.</span></h1>
+        <br>
+        <h1>coming soon<span class="dot">...</span></h1>
         <br>
         <br>
         <div class="description">
             <p>
-                This is my blog, where you'll find a variety of posts related to technology and development.
-                We will cover topics such as Laravel, Vue.js, software architecture, cloud computing, and career tips.
+                PHP, Laravel, Vue.js, Tall Stack, Cloud and Career tips.
             </p>
         </div>
         <br>
         <div class="icons">
-            <a href=""><i class="fa fa-twitter"></i></a>
-            <a href=""><i class="fa fa-instagram"></i></a>
-            <a href=""><i class="fa fa-linkedin"></i></a>
+            <a href="https://www.linkedin.com/in/lucassahdo"><i class="fa-brands fa-twitter"></i></a>
+            <a href="https://www.instagram.com/lucassahdo"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://twitter.com/lucassahdo"><i class="fa-brands fa-linkedin"></i></a>
         </div>
     </div>
 </x-app-layout>
